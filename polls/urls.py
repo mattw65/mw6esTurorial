054b2a1e5743a0627f0b160930aa0eca05d288bc
Matt Walsh <mattw6500@gmail.com>
2020-09-20 16:37:18 -0400

    First

---
+++ polls/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('comments', views.showform, name='comment'),
+    path('comments/list', views.CommentsView.as_view(), name='commentList'),
+]
+
+
